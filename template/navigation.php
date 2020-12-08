16c42f92dbe0fd1686817d218b6f127cc0ff8816
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-12-08 16:21:25 +0900

    update application name and add drawer menu

--- template/navigation.php
+++ template/navigation.php
@@ -5,7 +5,8 @@
 </ul>
 <nav>
   <div class="nav-wrapper blue">
-    <a href="#" class="brand-logo"><img src="../asset/images/chat.png" alt="app-logo"></a>
+    <a href="../index.php" id="app_name">備忘録掲示板</a>
+    <a href="#" data-target="drawer" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     <ul class="right hide-on-med-and-down">
       <li><a href="#">ホーム</a></li>
       <li><a href="#">投稿</a></li>
@@ -14,3 +15,8 @@
     </ul>
   </div>
 </nav>
+<ul class="sidenav" id="drawer">
+    <li><a href="">ホーム</a></li>
+    <li><a href="">投稿</a></li>
+    <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">メニュー<i class="material-icons right">arrow_drop_down</i></a></li>
+  </ul>
