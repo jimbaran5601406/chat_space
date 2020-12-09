ebae352e2ec0eee3ff12521435ced15aa9275c1f
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-12-09 16:02:03 +0900

    update navigation menu

--- asset/script.js
+++ asset/script.js
@@ -1,7 +1,3 @@
 $(document).ready(function () {
-	$(".dropdown-trigger").dropdown();
-});
-
-$(document).ready(function () {
 	$(".sidenav").sidenav();
 });
--- template/navigation.php
+++ template/navigation.php
@@ -1,22 +1,17 @@
-<ul id="dropdown1" class="dropdown-content">
-  <li><a href="#!">プロフィール</a></li>
-  <li class="divider"></li>
-  <li><a href="#!">ログアウト</a></li>
-</ul>
 <nav>
   <div class="nav-wrapper blue">
     <a href="../index.php" id="app_name">Chat Space</a>
-    <a href="#" data-target="drawer" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <a href="#" data-target="smp-drawer" class="sidenav-trigger"><i class="material-icons">menu</i></a>
     <ul class="right hide-on-med-and-down">
-      <li><a href="#">ホーム</a></li>
-      <li><a href="#">投稿</a></li>
-      <!-- Dropdown Trigger -->
-      <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">メニュー<i class="material-icons right">arrow_drop_down</i></a></li>
+      <li><a href="../index.php">ホーム</a></li>
+      <li><a href="../profile.php">プロフィール</a></li>
+      <li><a href="">ログアウト</a></li>
     </ul>
   </div>
 </nav>
-<ul class="sidenav" id="drawer">
-    <li><a href="">ホーム</a></li>
-    <li><a href="">投稿</a></li>
-    <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">メニュー<i class="material-icons right">arrow_drop_down</i></a></li>
-  </ul>
+
+<ul class="sidenav" id="smp-drawer">
+  <li><a href="../index.php">ホーム</a></li>
+  <li><a href="../profile.php">プロフィール</a></li>
+  <li><a href="">ログアウト</a></li>
+</ul>
