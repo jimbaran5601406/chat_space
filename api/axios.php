146be2b40750c2dfdeb0cba59cdaf765350510f0
KeiFunatsuya <jimbaran5601406@gmail.com>
2020-12-29 22:16:26 +0900

    modified live connection info

--- api/axios.php
+++ api/axios.php
@@ -1,14 +1,6 @@
 <?php
 require_once '../env.php';
-
-try {
-    $dsn = 'mysql:dbname=social-networking-service;host=localhost;charset=utf8';
-    $db_username = getenv('DB_USERNAME');
-    $db_password = getenv('DB_PASSWORD');
-    $db = new PDO($dsn, $db_username, $db_password);
-} catch(PDOException $e) {
-    echo('DB接続エラー：'.$e->getMessage());
-}
+require_once '../connect.php';
 
 $data = json_decode(file_get_contents("php://input"), TRUE);
 $id = $data['id'];
--- connect.php
+++ connect.php
@@ -1,10 +1,20 @@
 <?php
 
 try {
-    $dsn = 'mysql:dbname=social-networking-service;host=localhost;charset=utf8';
-    $db_username = getenv('DB_USERNAME');
-    $db_password = getenv('DB_PASSWORD');
-    $db = new PDO($dsn, $db_username, $db_password);
+    if($_SERVER["HTTP_HOST"] == 'chat_space.localhost') {
+        $db_name = getenv('DEV_DB_NAME');
+        $db_username = getenv('DEV_DB_USERNAME');
+        $db_password = getenv('DEV_DB_PASSWORD');
+        $dsn = 'mysql:dbname='.$db_name.';host=localhost;charset=utf8';
+        $db = new PDO($dsn, $db_username, $db_password);
+    } else {
+        $db_name = getenv('LIVE_DB_NAME');
+        $db_username = getenv('LIVE_DB_USERNAME');
+        $db_password = getenv('LIVE_DB_PASSWORD');
+        $db_host = getenv('LIVE_DB_HOST');
+        $dsn = 'mysql:dbname='.$db_name.';host='.$db_host.';charset=utf8';
+        $db = new PDO($dsn, $db_username, $db_password);
+    }
 } catch(PDOException $e) {
     echo('DB接続エラー：'.$e->getMessage());
 }
